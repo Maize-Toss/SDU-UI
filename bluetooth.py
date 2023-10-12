9f76574f61043e3c043f1f756733f12052b9c8cb
jlvargasme <jlvm.ind@hotmail.com>
2023-10-12 15:54:55 -0400

    add initial bluetooth setup

---
+++ bluetooth.py
@@ -0,0 +1,33 @@
+import bluetooth
+
+# Define the RFCOMM channel number
+channel = 0
+
+# Replace this with the device address you bound to
+device_address = "00:21:10:30:16:91"
+
+# Create a Bluetooth socket
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+try:
+    # Connect to the device
+    sock.connect((device_address, channel))
+    print("Connected to the device")
+
+    while True:
+        message = input("Enter a message to send (or 'q' to quit): ")
+        if message == 'q':
+            break
+
+        # Send the message
+        sock.send(message)
+
+        # Receive a response
+        data = sock.recv(1024)
+        print("Received:", data.decode())
+
+except bluetooth.btcommon.BluetoothError as e:
+    print("Error:", e)
+
+finally:
+    sock.close()
